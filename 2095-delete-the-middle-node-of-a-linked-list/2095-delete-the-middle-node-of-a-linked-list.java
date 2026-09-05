9996b1559329df4dc4af2b2da9cfdd2f14b92434
REDDY SARANYA <saranyareddy045@gmail.com>
2026-09-05 20:18:28 +0530

    Time: 3 ms (99.93%), Space: 202.8 MB (8.39%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -8,6 +8,9 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
+
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         ListNode slow=head;
@@ -27,6 +30,31 @@ class Solution {
             curr=curr.next;
         }
         curr.next=slow.next;
+        return head;
+    }
+}
+
+*/
+
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
         return head;
     }
 }
